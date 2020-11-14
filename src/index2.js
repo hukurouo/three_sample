c164cdda2c48ac27b06ccc94bee22161c7d652ee
hukurouo <owlbook248@gmail.com>
2020-11-14 20:58:40 +0900

    a

---
+++ src/index2.js
@@ -0,0 +1,82 @@
+import * as THREE from "three";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
+
+window.addEventListener("DOMContentLoaded", () => {
+  const VIEWPORT_W = window.innerWidth;
+  const VIEWPORT_H = window.innerHeight;
+
+  // レンダラーを作成
+  const renderer = new THREE.WebGLRenderer();
+  // レンダラーのサイズを設定
+  renderer.setSize(VIEWPORT_W, VIEWPORT_H);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  // canvasをbodyに追加
+  document.body.appendChild(renderer.domElement);
+
+  // シーンを作成
+  const scene = new THREE.Scene();
+
+  // カメラを作成
+  const camera = new THREE.PerspectiveCamera(
+    45,
+    VIEWPORT_W / VIEWPORT_H,
+    1,
+    1000
+  );
+
+  camera.position.set(10, 10, 10);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  var grid = new THREE.GridHelper(100, 10);
+  scene.add(grid);
+
+  const mtlLoader = new MTLLoader();
+  mtlLoader.setPath('./models/');
+  mtlLoader.load('switch.mtl', (materials) => {
+    materials.preload();
+    console.log(materials)
+    const objLoader = new OBJLoader();
+    objLoader.setMaterials(materials);
+    objLoader.setPath('./models/');
+    objLoader.load('switch.obj', (object) => {
+      const mesh = object;
+      scene.add(mesh);
+  
+    });
+  })
+  
+
+  // 箱を作成
+  const geometry = new THREE.BoxGeometry(11, 0.1, 3);
+  const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
+  const box = new THREE.Mesh(geometry, material);
+  box.position.z = -5;
+  scene.add(box);
+
+  // 平行光源を生成
+  const light = new THREE.DirectionalLight(0xffffff);
+  light.position.set(1, 1, 1);
+  scene.add(light);
+
+  controls.update();
+
+
+
+  
+  const tick = () => {
+    requestAnimationFrame(tick);
+
+    controls.update();
+
+
+    box.rotation.x += 0.005;
+    box.rotation.y += 0.005;
+
+    // 描画
+    renderer.render(scene, camera);
+  };
+  tick();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   // ローカル開発用環境を立ち上げる
   // 実行時にブラウザが自動的に localhost を開く
@@ -5,14 +7,29 @@ module.exports = {
     contentBase: "dist",
     open: true
   },
-  entry: "./src/index.js",
+  entry: {
+    'page1': './src/index.js',
+    'page2': './src/index2.js'
+  },
   // ファイルの出力設定
   output: {
     //  出力ファイルのディレクトリ名
     path: `${__dirname}/dist`,
     // 出力ファイル名
-    filename: "main.js"
+    filename: '[name].js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+      chunks: ['page1'],
+      filename: 'page1.html'
+    }),
+    new HtmlWebpackPlugin({
+      viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+      chunks: ['page2'],
+      filename: 'page2.html'
+    })
+  ],
   module: {
     rules: [
       {
