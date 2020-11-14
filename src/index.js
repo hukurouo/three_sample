e35c4b29fb5e0a57138876bbc88f0b7010a817aa
hukurouo <owlbook248@gmail.com>
2020-11-14 18:41:02 +0900

    ab

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import * as THREE from "three";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
 
 window.addEventListener("DOMContentLoaded", () => {
   const VIEWPORT_W = window.innerWidth;
@@ -23,8 +26,31 @@ window.addEventListener("DOMContentLoaded", () => {
     1000
   );
 
+  camera.position.set(0, 0, 1);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  const mtlLoader = new MTLLoader();
+  mtlLoader.setPath('./models/');
+  mtlLoader.load('switch.mtl', (materials) => {
+    materials.preload();
+    console.log(materials)
+    const objLoader = new OBJLoader();
+    objLoader.setMaterials(materials);
+    objLoader.setPath('./models/');
+    objLoader.load('switch.obj', (object) => {
+      console.log(object)
+      objmodel = object.clone();
+      objmodel.scale.set(100, 100, 100);
+      const mesh = objmodel;
+      this.scene.add(mesh);
+  
+    });
+  })
+  
+
   // 箱を作成
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const geometry = new THREE.BoxGeometry(1, 0.1, 1);
   const material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
   const box = new THREE.Mesh(geometry, material);
   box.position.z = -5;
@@ -35,11 +61,19 @@ window.addEventListener("DOMContentLoaded", () => {
   light.position.set(1, 1, 1);
   scene.add(light);
 
+  controls.update();
+
+
+
+  
   const tick = () => {
     requestAnimationFrame(tick);
 
-    box.rotation.x += 0.05;
-    box.rotation.y += 0.05;
+    controls.update();
+
+
+    box.rotation.x += 0.005;
+    box.rotation.y += 0.005;
 
     // 描画
     renderer.render(scene, camera);
